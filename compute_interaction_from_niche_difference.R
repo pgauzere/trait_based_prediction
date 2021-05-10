46ceb310cc8d8482b8677cdc91ee4bc355d60735
Gaüzère <pierre.gauzere@gmail.com>
2021-05-10 10:30:54 +0200

    update function niche difference from Ben's version

--- compute_interaction_from_niche_difference.R
+++ compute_interaction_from_niche_difference.R
@@ -1,6 +1,5 @@
 niche_difference      <- function(x) {
   ifelse(x$trait.i != x$trait.j, 
-         -1/(1 + x$env) * abs(x$trait.i - x$trait.j) / #this is the function that matters
-           abs(min(-1/(1 + x$env) * abs(x$trait.i - x$trait.j))),
+        (-1 / (1 + x$env)) * abs (1 / (x$trait.i - x$trait.j)),
          -1)
 }
