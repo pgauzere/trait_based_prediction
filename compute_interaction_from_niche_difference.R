dea96bba455791bb4891a181afaa4cb070d6f0e3
Gaüzère <pierre.gauzere@gmail.com>
2021-06-11 12:58:23 +0200

    Update function with threshold

--- compute_interaction_from_niche_difference.R
+++ compute_interaction_from_niche_difference.R
@@ -1,5 +1,12 @@
-niche_difference      <- function(x) {
-  ifelse(x$trait.i != x$trait.j, 
-        (-1 / (1 + x$env)) * abs (1 / (x$trait.i - x$trait.j)),
-         -1)
+niche_difference <- function(x) {
+trait_diff <- abs(x$trait.i - x$trait.j)
+
+threshold <- quantile(trait_diff, probs = 0.1)
+
+trait_diff[trait_diff < threshold] <- threshold
+
+ifelse(x$trait.i != x$trait.j,
+       -1/((1 + x$env) * trait_diff) / #this is the function that matters
+         abs(min(-1/((1 + x$env) * trait_diff))),
+       -1)
 }
