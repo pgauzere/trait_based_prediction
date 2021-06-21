e1d3e2c08ee2d56f85aad81f39d2dc38abf0d6ea
bblonder <bblonder@gmail.com>
2021-06-21 06:02:25 -0700

    niche difference updates

--- compute_interaction_from_niche_difference.R
+++ compute_interaction_from_niche_difference.R
@@ -1,12 +1,19 @@
+niche_difference_non_normalized <- function(x) {
+  ifelse(x$trait.i != x$trait.j, 
+         (-1 / (1 + x$env)) * abs (1 / (x$trait.i - x$trait.j)),
+         -1)
+}
+
+
 niche_difference <- function(x) {
-trait_diff <- abs(x$trait.i - x$trait.j)
-
-threshold <- quantile(trait_diff, probs = 0.1)
-
-trait_diff[trait_diff < threshold] <- threshold
-
-ifelse(x$trait.i != x$trait.j,
-       -1/((1 + x$env) * trait_diff) / #this is the function that matters
-         abs(min(-1/((1 + x$env) * trait_diff))),
-       -1)
+  trait_diff <- abs(x$trait.i - x$trait.j)
+  
+  threshold <- quantile(trait_diff, probs = 0.1)
+  
+  trait_diff[trait_diff < threshold] <- threshold
+  
+  ifelse(x$trait.i != x$trait.j,
+         -1/((1 + x$env) * trait_diff) / #this is the function that matters
+           abs(min(-1/((1 + x$env) * trait_diff))),
+         -1)
 }
