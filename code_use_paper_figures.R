85405981a5eea5b1fa9630f4e281e354d3a97d09
Gaüzère <pierre.gauzere@gmail.com>
2022-12-09 14:02:45 +0100

    code for revised version of the paper

--- code_use_paper_figures.R
+++ code_use_paper_figures.R
@@ -6,6 +6,7 @@
 library(viridis) #because we like nice color scales
 library(see)
 
+
 ############### A Predicting demographic model parameters ######################
 
 #We will use the function predict_model_parameters()
@@ -40,7 +41,7 @@ alpha.df %>% filter( i != j) %>% #remove intraspecific coefficient
 
 
 
-##### Figure 4a #####
+##### Figure S2 #####
 ## we simulate 10replicates along a continuous environmental gradient 
 # under the competitive dominance hypothesis of the niche difference
 alpha.df <- predict_demographic_model_parameters(
@@ -68,10 +69,9 @@ ggplot(aes(x = i, y = j, fill=interaction.coef))+
 source("predict_coexistence_outcome.R")
 # read the forewords of the function to know how to use it
 
-### Figure 4b-c : plot networks #####
-
+### plot networks #####
 # we can use predict_demographic_model_parameters() with only 2 environmental values
-# choose environmental value : env = 0 for fig 4.b, env = 10 for fig 4.c
+# choose environmental value : env = 0  or env = 10
 alpha.df <- predict_demographic_model_parameters(
   rep = 1,
   nsp = 10,
@@ -88,9 +88,6 @@ alpha.df %>%
 alpha.df %>% 
   filter(env == 10) %>%
   predict_coexistence_outcome(plot.network = T, network.threshold = 0.05)
-
-
-# note that color scale in paper Figure 4 is adjusted limits[-1,0] to facilitate comparison between network 
 
 
 #### the function compute a lot of network features and coexistence, but is time demanding 
@@ -123,7 +120,7 @@ alpha.properties <-
 
 ## We can play to see links with community or matrix properties and coexistence outcomes
 
-#feasibility only achived at low environmental values (less stressfull conditions)
+#feasibility only achieved at low environmental values (less stressful conditions)
 ggplot(alpha.properties, aes(x = env, y = mean_trait))+
   geom_point(aes(col = numeric_feasibility, alpha = n_realized))+
   scale_color_viridis_d()+
@@ -131,36 +128,35 @@ ggplot(alpha.properties, aes(x = env, y = mean_trait))+
   theme(legend.position = "bottom")
 
 # environment stress is linked with reduced richness and trait variability 
-ggplot(alpha.properties, aes(x = env, y = cwv))+
+ggplot(alpha.properties, aes(x = env, y = mean_trait))+
   geom_point(aes(col = n_realized))+
   scale_color_viridis()+
   theme_classic()+
   theme(legend.position = "bottom")
 
-# and the first moment of the mean interspecific interaction coeff distribution is linked with realized community size 
+# and the first moment of the mean interspecific interaction coefficient distribution is linked with realized community size 
 ggplot(alpha.properties, aes(x = E1, y = n_realized))+
   geom_point()+
   scale_color_viridis_d()+
   theme_classic()
 
-##### Figure 5 coexistence outcome surface  #####
+##### Figure 2 coexistence outcome surface  #####
 # first, predict model parameters. It is better to have many replicates in order sample large variation in trait mean
 #because they  are randomly sampled from distribution)
 source("predict_demographic_model_parameters.R")
 
 alpha.df <- predict_demographic_model_parameters(
   nsp = 10, 
-  mechanism ="niche difference", 
+  mechanism ="competitive dominance", 
   trait.distribution ="uniform",
   rep =100,
-  env = seq(from = 0, to = 1, by = 0.01))
+  env = seq(from = 0, to = 10, by = 0.1))
 
 #then we can compute the coexistence outcome, but it can take a lot of time.
 source("predict_coexistence_outcome.R")
 
 # note that the coexistence outcomes are sensitive to initial conditions,
 # so to deal with that I advice keeping initial.abundance = "random" and using a lot of replicates
-
 alpha.properties <-
   alpha.df %>%
   group_by(env, replicat, trait.distribution, mechanism) %>%
@@ -173,16 +169,13 @@ alpha.properties <-
                                               plot.dynamic = F,
                                               plot.network = F))
 
-
-
-# To create figure 5, we interpolate the predicted over the full surface of trait - env
-
+# To create figure 2, we interpolate the predicted over the full surface of trait - env
 ## feasability probability
 library(akima)
 feasibility_interpolation <-
   interp(
     alpha.properties$env,
-    alpha.properties$var_trait,
+    alpha.properties$mean_trait,
     as.factor(alpha.properties$analytic_feasibility),
     nx = 100,
     ny = 100,
@@ -192,7 +185,8 @@ li.zmin <- min(feasibility_interpolation$z,na.rm=TRUE)
 li.zmax <- max(feasibility_interpolation$z,na.rm=TRUE)
 breaks <- pretty(c(li.zmin,li.zmax),10)
 colors <- viridis(length(breaks)-1)
-with(feasibility_interpolation, image (x,y,z, breaks=breaks, col=colors, ylim = c(0.05, 0.12)))
+with(feasibility_interpolation, image (x,y,z, breaks=breaks, col=colors))
+# contour(feasibility_interpolation, levels=1.01, add=TRUE, col = "white")
 
 ##stability
 stab_interpolation <-
@@ -208,8 +202,8 @@ li.zmin <- min(stab_interpolation$z,na.rm=TRUE)
 li.zmax <- max(stab_interpolation$z,na.rm=TRUE)
 breaks <- pretty(c(li.zmin,li.zmax),10)
 colors <- viridis(length(breaks)-1)
-with(stab_interpolation, image (x,y,z, breaks=breaks, col=colors, ylim = c(0.3, 0.7)))
-# contour(stab_interpolation, levels=breaks, add=TRUE)
+with(stab_interpolation, image (x,y,z, breaks=breaks, col=colors))
+# contour(stab_interpolation, levels=1.01, add=TRUE, col = "white")
 
 
 ##n_realized
@@ -219,7 +213,12 @@ li.zmax <- max(n_interpolation$z,na.rm=TRUE)
 breaks <- pretty(c(li.zmin,li.zmax),10)
 colors <- viridis(length(breaks)-1)
 with(n_interpolation, image (x,y,z, breaks=breaks, col=colors, ylim = c(0.3, 0.7)))
- # contour(n_interpolation, levels=breaks, add=TRUE)
+# contour(n_interpolation, levels=breaks, add=TRUE)
+
+
+
+
+
 
 
 ############### C Predicting invasibility ############### 
@@ -229,12 +228,11 @@ with(n_interpolation, image (x,y,z, breaks=breaks, col=colors, ylim = c(0.3, 0.7
  source("predicting_invasibility.R")
  # read the forewords of the function to know how to use it
 
-
-##### Figure  6 #####
+##### Figure  3 #####
 res_invasion <-
    predict_invasibility_surface(
      nsp = 10,
-     env = seq(from = 0, to = 10, by = 0.1),
+     env = seq(from = 1, to = 10, by = 1),
      trait.distribution = c("uniform"),
      mechanism = "niche difference",
      Nmin = 0.0001 ,
@@ -242,12 +240,12 @@ res_invasion <-
      growth.rate = 0.5,
      n.time.step = 250,
      extinction = T,
-     plot.invasion = F,
-     traits_to_test = seq(0,1, 0.01), 
+     plot.invasion = T,
+     traits_to_test = seq(0,1, 0.2), 
      plot.surface = T
    )
 
-# to make the exact figure 6 increase the definition of traits_to_test and env vector by 10 
+# to make the exact figure 3 increase the definition of traits_to_test and env vector by 10 
 
 ############### D Predicting environmental change response ###############
 
@@ -298,13 +296,24 @@ source("simulate_environmental_change.R")
 source("predict_environmental_change_response.R")
 # read the forewords of the function to know how to use it
 
-##### Figure  7 #####
-
+##### Figure  4 #####
 #simulate linear temporal increase in environment 
-linear_change <- simulate_environmental_change(type = "linear", trend_value = 3, plot.env.change = T)
+linear_change <- simulate_environmental_change(n.time.step = 1000, type = "linear", trend_value = 3, plot.env.change = T)
 
+pdf("predict_env_response.pdf", width= 4,  height = 6)
 # choose mechanism = "competitive dominance" for Fig7a-b and mechanism = "niche difference" for Fig7c-d 
-linear_change_response <- predict_environmental_change_response(nsp = 10,
+linear_change_response_nicheDiff <- predict_environmental_change_response(nsp = 10,
+                                                                        env.change = linear_change,
+                                                                        trait.distribution = "uniform",
+                                                                        mechanism = "niche difference",
+                                                                        Nmin = 0.001,
+                                                                        initial.abundance = 0.005,
+                                                                        growth.rate =0.5,
+                                                                        extinction = T,
+                                                                        plot.species.dynamics = F,
+                                                                        plot.response.diagram = T)
+
+linear_change_response_compDom <- predict_environmental_change_response(nsp = 10,
                                               env.change = linear_change,
                                               trait.distribution = "uniform",
                                               mechanism = "competitive dominance",
@@ -312,22 +321,36 @@ linear_change_response <- predict_environmental_change_response(nsp = 10,
                                               initial.abundance = 0.005,
                                               growth.rate =0.5,
                                               extinction = T,
-                                              plot.species.dynamics = T,
+                                              plot.species.dynamics = F,
                                               plot.response.diagram = T)
 
 
-cyclic.change <- simulate_environmental_change(type = "cyclic", cycle_value = 10, cycle_period = 0.1, plot.env.change = F)
-cyclic_change_response <- predict_environmental_change_response(nsp = 10,
+cyclic.change <- simulate_environmental_change(n.time.step = 1000, type = "cyclic", cycle_value = 10, cycle_period = 0.025, plot.env.change = F)
+cyclic_change_response_nicheDiff <- predict_environmental_change_response(nsp = 10,
                                                           env.change = cyclic.change,
                                                           trait.distribution = "uniform",
-                                                          mechanism =  "competitive dominance",
+                                                          mechanism =  "niche difference",
                                                           Nmin = 0.001,
                                                           initial.abundance = 0.005,
                                                           growth.rate = 0.5,
                                                           extinction = T,
-                                                          plot.species.dynamics = T,
+                                                          plot.species.dynamics = F,
                                                           plot.response.diagram = T)
 
+
+cyclic_change_response_compDom <- predict_environmental_change_response(nsp = 10,
+                                                                env.change = cyclic.change,
+                                                                trait.distribution = "uniform",
+                                                                mechanism =  "competitive dominance",
+                                                                Nmin = 0.001,
+                                                                initial.abundance = 0.005,
+                                                                growth.rate = 0.5,
+                                                                extinction = T,
+                                                                plot.species.dynamics = F,
+                                                                plot.response.diagram = T)
+
+
+dev.off()
 
 ##### Figure  8 #####
 cyclic.change <- simulate_environmental_change(type = "cyclic", cycle_value = 10, cycle_period = 0.05, 
